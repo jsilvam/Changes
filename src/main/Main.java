1f5ba683985d57537c5c5b35383b3adfbc9cfe0a
Jaziel Moreira <Jaziel Moreira@150.165.98.179>
2017-08-10 10:13:01 -0300

    Signed-off-by: Jaziel Moreira <Jaziel Moreira@150.165.98.179>

---
+++ src/main/Main.java
@@ -0,0 +1,36 @@
+package main;
+
+import java.io.File;
+import java.util.List;
+
+import ch.uzh.ifi.seal.changedistiller.ChangeDistiller;
+import ch.uzh.ifi.seal.changedistiller.ChangeDistiller.Language;
+import ch.uzh.ifi.seal.changedistiller.distilling.FileDistiller;
+import ch.uzh.ifi.seal.changedistiller.model.entities.SourceCodeChange;
+
+public class Main {
+
+	public static void main(String[] args) {
+		File left = new File("Purity1.java");
+		File right = new File("Purity2.java");
+
+		FileDistiller distiller = ChangeDistiller.createFileDistiller(Language.JAVA);
+		try {
+		    distiller.extractClassifiedSourceCodeChanges(left, right);
+		} catch(Exception e) {
+		    /* An exception most likely indicates a bug in ChangeDistiller. Please file a
+		       bug report at https://bitbucket.org/sealuzh/tools-changedistiller/issues and
+		       attach the full stack trace along with the two files that you tried to distill. */
+		    System.err.println("Warning: error while change distilling. " + e.getMessage());
+		}
+
+		List<SourceCodeChange> changes = distiller.getSourceCodeChanges();
+		if(changes != null) {
+		    for(SourceCodeChange change : changes) {
+		        System.out.println(change.getLabel());
+		    }
+		}
+
+	}
+
+}
