62bdd72da035217a909cdd604bb483066f89bd3a
Jaziel Moreira <Jaziel Moreira@DESKTOP-AU1TP7B>
2017-08-11 02:47:23 -0300

    Signed-off-by: Jaziel Moreira <Jaziel Moreira@DESKTOP-AU1TP7B>

--- src/main/Changes.java
+++ src/main/Changes.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import ch.uzh.ifi.seal.changedistiller.ChangeDistiller;
@@ -12,42 +13,65 @@ import utils.*;
 public class Changes {
 	
 	private String urlRepository;
+	private String csvPath;
 	
-	public Changes(String urlRepository){
+	public Changes(String urlRepository, String csvPath){
 		this.urlRepository=urlRepository;
+		this.csvPath=csvPath;
 	}
 	
-	public void check(String commit,String parent) throws Exception {
-		Refactorings refactorings=new Refactorings("","");
-		GithubDownloader git=new GithubDownloader(urlRepository);
-		List<SourceCodeChange> changes;
+	public List<SourceCodeChange> getChanges(String commit) throws Exception {
+		//initialize
+		Refactorings refactorings = new Refactorings(this.csvPath,commit); //class not finished.
+		String parent=refactorings.getParent();
+		GithubDownloader git = new GithubDownloader(this.urlRepository);
+		List<SourceCodeChange> changes = new ArrayList<SourceCodeChange>();
 		
+		//download and extract projects
 		File targetFile=git.downloadCommit(commit);
 		File targetFolder=ZipExtractor.extract(targetFile, new File(git.getLocation(),commit));
-		
-		
 		File sourceFile=git.downloadCommit(parent);
 		File sourceFolder=ZipExtractor.extract(sourceFile, new File(git.getLocation(),parent));
 		
-		List<String> sourceFiles=FileUtils.listClass(sourceFolder);
-		List<String> targetFiles=FileUtils.listClass(targetFolder);
+		//list Java files
+		List<String> sourceFiles=FileUtils.listClass(new File(sourceFolder,"src\\main\\java"));
+		List<String> targetFiles=FileUtils.listClass(new File(targetFolder,"src\\main\\java"));
 		
 		for(String file:sourceFiles) {
-			if(refactorings.getChangedClasses().containsKey(file)) {
-				FileDistiller distiller = ChangeDistiller.createFileDistiller(Language.JAVA);
-				try {
-				    //distiller.extractClassifiedSourceCodeChanges(file, refactorings.getChangedClasses().get(file));
-				} catch(Exception e) {
-				    System.err.println("Warning: error while change distilling. " + e.getMessage());
-				}
-
-				changes = distiller.getSourceCodeChanges();
-			}
+			File left = new File(sourceFolder,"src\\main\\java"+file.replace(".", "\\"));
+			if(refactorings.getChangedClassSignatures().containsKey(file)) {
+				File right = new File(targetFolder,"src\\main\\java"+refactorings.getChangedClassSignatures().get(file).replace(".", "\\"));
+				changes.addAll(listChangesOfClass(left, right));
+			}else if(targetFiles.contains(file)){
+				File right = new File(targetFolder,"src\\main\\java"+file.replace(".", "\\"));
+				changes.addAll(listChangesOfClass(left, right));
+			}else
+				System.out.println("File Removed: "+file);
 		}
 		
+		//Remove the files that were already analyzed, remaining only the new files
+		targetFiles.removeAll(sourceFiles);
+		
+		for(String file: targetFiles)
+			System.out.println("File Removed: "+file);
+		
+		System.out.println("\n\nChanges...\n");
 		
 		deleteDirectory(git.getLocation());
 		
+		return changes;
+		
+	}
+	
+	public List<SourceCodeChange> listChangesOfClass(File left, File right) {
+		FileDistiller distiller = ChangeDistiller.createFileDistiller(Language.JAVA);
+		try {
+		    distiller.extractClassifiedSourceCodeChanges(left, right);
+		} catch(Exception e) {
+		    System.err.println("Warning: error while change distilling. " + e.getMessage());
+		}
+		List<SourceCodeChange> changes = distiller.getSourceCodeChanges();
+		return changes;
 	}
 	
 	private void deleteDirectory(File dir){
--- src/utils/Refactorings.java
+++ src/utils/Refactorings.java
@@ -11,17 +11,30 @@ import java.util.Scanner;
 
 public class Refactorings {
 	
-	private Map<String, String> changedClasses;
+	private Map<String, String> changedClassSignatures;
+	private List<String> addedClasses;
+	private List<String> changedMethods;
+	private List<String> addedMethods;
+	private List<String> removedMethods;
+	private List<String> movedAtributes;
+	
+	private String parent;
 	
 	public Refactorings(String csvPath,String commit) throws FileNotFoundException {
+		init(csvPath, commit);
+	}
+	
+	private void init(String csvPath,String commit) throws FileNotFoundException {
 		
 		Scanner in = new Scanner(new FileReader(csvPath)).useDelimiter(";");
 		
+		boolean flag=false;  //responsible for stopping the loop when there is no more entries of the selected commit
 		in.nextLine();
 		while(in.hasNext()) {
 			in.next();
 			if(in.next().equals(commit)) {
-				in.next();
+				flag=true;
+				this.parent=in.next();
 				in.next();
 				in.next();
 				String refactoring=in.next();
@@ -29,22 +42,34 @@ public class Refactorings {
 					String key=in.next();
 					in.next();
 					String value=in.next();
-					changedClasses.put(key, value);
+					changedClassSignatures.put(key, value);
 				}else if(refactoring.equals("Move Class")) {
 					in.next();
 					String key=in.next();
 					in.next();
 					String value=in.next();
-					changedClasses.put(key, value);
+					changedClassSignatures.put(key, value);
+				}else if(refactoring.equals("Move And Rename Class")) {
+					in.next();
+					String key=in.next();
+					in.next();
+					String value=in.next();
+					changedClassSignatures.put(key, value);
 				}
-			}
+			}else 
+				if(flag)break;
+			
 			in.nextLine();
 		}
 		in.close();
 	}
+	
+	public String getParent() {
+		return this.parent;
+	}
 
-	public Map<String, String> getChangedClasses() {
-		return changedClasses;
+	public Map<String, String> getChangedClassSignatures() {
+		return this.changedClassSignatures;
 	}
 	
 	
